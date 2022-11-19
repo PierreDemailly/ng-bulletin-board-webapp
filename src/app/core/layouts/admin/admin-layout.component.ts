3f45d627f5a2209d6504e7912b86fe0069f94259
PierreDemailly <pierredemailly.pro@gmail.com>
2022-11-19 15:29:30 +0100

    fix theme

--- src/app/core/layouts/admin/admin-layout.component.ts
+++ src/app/core/layouts/admin/admin-layout.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { SidenavService } from '@services/sidenav.service';
+import { AlertService } from '@shared/services/alert.service';
+import { SettingsService } from '@shared/services/settings.service';
 
 /**
  * Admin layout.
@@ -16,7 +18,7 @@ export class AdminLayoutComponent implements OnInit {
   /**
    * Creates an instance of AdminLayoutComponent.
    */
-  constructor(private sidenavService: SidenavService) { }
+  constructor(private sidenavService: SidenavService, private settingsService: SettingsService, private alertService: AlertService) { }
 
   /**
    * @inheritdoc
@@ -27,6 +29,23 @@ export class AdminLayoutComponent implements OnInit {
     // if the main sidenav is open, close it when coming into admin
     if (sidenav.opened) {
       this.sidenavService.toggle();
+    }
+    
+    this.getSettings();
+  }
+
+  /**
+   * Get settings and store them in subject so they can be used in the admin modules.
+   */
+  async getSettings() {
+    try {
+      const settings = await this.settingsService.getAll();
+      console.log(settings);
+      this.settingsService.setSettings(settings);
+    } catch (e) {
+      // notify
+      this.alertService.warn('Impossible de récupérer les paramètres de l\'application, veuillez recharger la page.');
+      throw new Error(e);
     }
   }
 }
